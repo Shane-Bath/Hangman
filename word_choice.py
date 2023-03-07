2ed17876810a0f5d6e8dae45d8645657d2385815
Shane-Bath <shane.bath@gmail.com>
2023-03-07 22:43:37 +0000

    Fixed Errorname random_word, by assign a veriable to the function. Not sure if this will cause problem later

--- word_choice.py
+++ word_choice.py
@@ -17,22 +17,48 @@ def select_word():
     data = words.get_all_values()
     list_words = words.col_values(1)
     random_word = random.choice(list_words).lower()
-    print(random_word)
+    return random_word
 
-'''
+selected_word = select_word()
+print(selected_word)
+
+#NameError: name 'random_word' is not defined. is it outside the function
+
+
+
 # selected_word  list(random_word)
 # split = random_word.split() not using 
-selected_word = [*random_word]
+
 # print(random_word)
-print(selected_word)
+# print(selected_word)
 # print(split) not using 
-placeholder = "_"*len(selected_word)
-print(f"{list(placeholder)}")
-'''
-'''
+# placeholder = "_"*len(selected_word)
+# print(f"{list(placeholder)}")
+
+# invite player to guess the letter
 user_input = input("Guess a letter: ")
-print(user_input)
-'''
+# I have to save the letter to a variable?
+letter_guess = user_input.lower()
+print(letter_guess)
+
+# check of the letter is in word, if not record wrong guess
+# Need to keep count of guess, set max number of guess or min number of guess
+# if right guess, print the letter
+# if incorrect add to wrong guess and draw hangman
+wrong_guesses = 0
+
+for letter in random_word:
+    if letter in letter_guess:
+        print(f"{letter_guess}")
+    else:
+        print("_")
+
+
+
+
+# if wrong_guesses == 4:
+#     break 
+# print("Game over!") 
 
 """
 create a loop to match the user input with a letter in the word and replace the blank
