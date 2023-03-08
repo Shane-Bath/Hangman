72df4bfa2375dbc1ab83eb39d2185a6c364cee1a
Shane-Bath <shane.bath@gmail.com>
2023-03-08 21:31:41 +0000

    create acsii.py and tile graphic

---
+++ acsii.py
@@ -0,0 +1,26 @@
+title = """
+  _    _                                         
+ | |  | |                                        
+ | |__| | __ _ _ __   __ _ _ __ ___   __ _ _ __  
+ |  __  |/ _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+ | |  | | (_| | | | | (_| | | | | | | (_| | | | |
+ |_|  |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                      __/ |                      
+                     |___/           
+"""
+
+
+stage_one = """
+|======
+|     |
+|
+|
+|
+|
+|
+============"""
+
+print(title)
+
+#SyntaxError: EOL while scanning string literal
+# https://datagy.io/python-syntax-error-eol-while-scanning-string-literal/
