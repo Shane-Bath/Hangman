21c6fbaeca41a334dea56578b94679a39d52fcbd
Shane-Bath <shane.bath@gmail.com>
2023-03-08 21:56:24 +0000

    Complete ascii graphic for each stage

--- acsii.py
+++ acsii.py
@@ -11,16 +11,78 @@ title = """
 
 
 stage_one = """
-|======
-|     |
+
++=======+
+|       
+|      
 |
 |
 |
 |
++==========="""
+
+stage_two = """
+
++=======+
+|       |
+|      {OO}
+|       - 
 |
-============"""
+|
+|
++==========="""
+
+stage_three = """
+
++=======+
+|       |   
+|      {OO}/
+|       - /
+|      / | 
+|     / 
+|
++==========="""
+
+stage_four = """
+
++=======+
+|       |
+|      {OO}/
+|       - /
+|      / | 
+|     / [ ] 
+|       /  \ 
++==========="""
+
+stage_five = """
++=======+
+|       |
+|      {xx}
+|       o 
+|      / | |
+|     / [ ] 
+|       /  \ 
++==========="""
+game_over = """
+   _____                                            
+  / ____|                                           
+ | |  __  __ _ _ __ ___   ___    _____   _____ _ __ 
+ | | |_ |/ _` | '_ ` _ \ / _ \  / _ \ \ / / _ \ '__|
+ | |__| | (_| | | | | | |  __/ | (_) \ V /  __/ |   
+  \_____|\__,_|_| |_| |_|\___|  \___/ \_/ \___|_|   
+                                                    
+                                                    
+"""
+
 
 print(title)
+
+print(stage_one)
+print(stage_two)
+print(stage_three)
+print(stage_four)
+print(stage_five)
+print(game_over)
 
 #SyntaxError: EOL while scanning string literal
 # https://datagy.io/python-syntax-error-eol-while-scanning-string-literal/
