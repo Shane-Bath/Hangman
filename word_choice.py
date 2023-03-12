f0e3e02bca743f72036a6e88b12c2950dc0fb597
Shane-Bath <shane.bath@gmail.com>
2023-03-12 17:40:31 +0000

    add if not statement and import acsii

--- word_choice.py
+++ word_choice.py
@@ -1,11 +1,16 @@
 import gspread
 from google.oauth2.service_account import Credentials
 import random
-from acsii import game_over
+from acsii import title
 from acsii import stage_one
+from acsii import stage_two
+from acsii import stage_three
+from acsii import stage_four
+from acsii import stage_five
+
 # from acsii import title
 #  displaying all the acsii graphics? why
-
+print(title)
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -26,8 +31,8 @@ def select_word():
 
 selected_word = select_word()
 print(selected_word)
-wrong_guesses = 4
-# number of guesses allowed 4
+wrong_guesses = 5
+# number of guesses allowed 5
 
 # create a list. 
 placeholder = []
@@ -37,20 +42,54 @@ for letter in selected_word:
     placeholder += "_"
 print(placeholder)
 
+
 while wrong_guesses > 0:
     user_input = input("Guess a letter: ")
     letter_guess = user_input.lower()
     if letter_guess in selected_word:
         for i in range(len(selected_word)):
             if selected_word[i] == letter_guess:
-                 placeholder[i] = letter_guess
-        
+                placeholder[i] = letter_guess
+                
         print("Correct")
         print(placeholder)
     else:
         wrong_guesses -= 1
-        print(f"You have {wrong_guesses} lives left.")
-       
+                 
+        
+
+# elif wrong_guesses == 3:
+#     print(stage_two)
+# elif wrong_guesses == 2:
+#     print(stage_three)
+# elif wrong_guesses == 1:
+#     print(stage_four)
+# else:
+#     wrong_guesses == 0
+#     print(stage_five)
+
+
+#  wrong_guesses == 4
+#         print(f"{stage_one}")
+#         wrong_guesses == 3
+#         print(f"{stage_two}")
+#         wrong_guesses == 2
+#         print(f"{stage_three}")
+#         wrong_guesses == 1
+#         print(f"{stage_four}")
+#         wrong_guesses == 0
+#         print(f"{stage_five}")
+# print(f"You have {wrong_guesses} lives left.")
+
+
+# if wrong_guesses == 4:
+#     print(f"{stage_one}")
+#     print(f"You have {wrong_guesses} lives left.")
+
+
+
+
+
 # issues and errors with the above code
 # 1. While loop continues even if all letters guessed
 #    need to stop while loop if all letter guessed
@@ -130,3 +169,4 @@ offer player to restart game or quit
 # 6.1 only lower case letter and not number
 # 7 run the game while loop (solved)
 # 8 looping through list, and replacing _ if match is throwing up an error - see bug
+# 9 end game once all the letters have been guessed.
